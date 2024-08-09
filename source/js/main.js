967998bf3628e56c686e174ab912b446fbe35bad
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2024-08-09 14:29:38 +0500

    utils

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,12 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+// import {iosVhFix} from './utils/ios-vh-fix';
+// import {initModals} from './modules/modals/init-modals';
+// import {Form} from './modules/form-validate/form';
+// import {initTabs} from './modules/tabs/init-tabs';
+// import {initAccordions} from './modules/accordion/init-accordion';
+// import {slider} from './modules/slider';
+// import {slider2} from './modules/slider';
+// import {initVideo} from './modules/video';
