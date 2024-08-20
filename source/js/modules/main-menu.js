1893fc9df71f1b0bfb86f3fa2e3fe537bb4c1264
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2024-08-20 07:43:07 +0500

    main menu

--- source/js/modules/main-menu.js
+++ source/js/modules/main-menu.js
@@ -1,6 +1,6 @@
 export const mobileMenu = function sliderGallery() {
   const menuButton = document.querySelector('.main-nav__toggle');
-  const mobiMenu = document.querySelector('.main-nav__list');
+  const mobiMenu = document.querySelector('.main-nav__menu');
   // const noJs = Array.from(document.querySelectorAll('.main-nav__nojs'));
 
   // for (let i = 0; i < 2; i++) {
@@ -8,7 +8,7 @@ export const mobileMenu = function sliderGallery() {
   // }
 
   menuButton.addEventListener('click', () =>{
-    mobiMenu.classList.toggle('main-nav__list--open');
+    mobiMenu.classList.toggle('main-nav__menu--open');
     menuButton.classList.toggle('main-nav__toggle--active');
   });
 };
